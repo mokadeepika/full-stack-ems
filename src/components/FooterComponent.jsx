92ce89dac52c24d694e2bf86147c7724f47d3cdf
mokadeepika <mokadeepika@gmail.com>
2024-04-01 14:01:57 -0400

    first commit

---
+++ src/components/FooterComponent.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const FooterComponent = () => {
+  return (
+    <footer className="footer">
+      <span className="text-center text-bold">
+        All rights reserved @Deepika Moka
+      </span>
+    </footer>
+  );
+};
+
+export default FooterComponent;
