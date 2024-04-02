7a25f13a9619f4e61358e392349e10ff994e2465
mokadeepika <47566803+mokadeepika@users.noreply.github.com>
2024-04-02 08:44:39 -0400

    DepartmentService.js

--- src/services/DepartmentService.js
+++ src/services/DepartmentService.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const DEPARTMENT_REST_API_BASE_URL = 'https://emsbackend-production.up.railway.app'
+const DEPARTMENT_REST_API_BASE_URL = 'https://emsbackend-production.up.railway.app/api/employees'
 
 export const getAllDepartments = () => axios.get(DEPARTMENT_REST_API_BASE_URL);
 
