110cc9038dab293579b210178d9d6038b97bee2c
mokadeepika <47566803+mokadeepika@users.noreply.github.com>
2024-04-01 22:28:05 -0400

    EmployeeService.js

--- src/services/EmployeeService.js
+++ src/services/EmployeeService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const REST_API_BASE_URL = 'https://emsbackend-production.up.railway.app/employees';
+const REST_API_BASE_URL = 'https://emsbackend-production.up.railway.app';
 
 export const listEmployees = () => axios.get(REST_API_BASE_URL);
 
